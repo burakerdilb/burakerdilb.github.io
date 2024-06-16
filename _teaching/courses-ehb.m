468dc3c1d8a16841bbf7602a2091f28dcde1af7b
burakerdilb <69845868+burakerdilb@users.noreply.github.com>
2024-06-17 01:48:36 +0300

    Update and rename 2015-spring-teaching-2.md to courses-ehb.m

--- _teaching/courses-ehb.m
+++ _teaching/courses-ehb.m
@@ -1,11 +1,7 @@
 ---
-title: "Teaching experience 2"
+title: "Electronics and Communications Engineering Courses"
 collection: teaching
-type: "Workshop"
-permalink: /teaching/2015-spring-teaching-1
-venue: "University 1, Department"
-date: 2015-01-01
-location: "City, Country"
+permalink: /courses/ehb-courses
 ---
 
 This is a description of a teaching experience. You can use markdown like any other post.
